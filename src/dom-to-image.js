3be5e8b3f770c5d22ff108277adc9357294a6085
Nick Day <nick@njday.com>
2018-11-08 16:21:30 +0000

    retina support

--- src/dom-to-image.js
+++ src/dom-to-image.js
@@ -155,14 +155,18 @@
             .then(util.delay(100))
             .then(function (image) {
                 var canvas = newCanvas(domNode);
-                canvas.getContext('2d').drawImage(image, 0, 0);
+                var ctx = canvas.getContext('2d');
+                ctx.scale(2, 2);
+                ctx.drawImage(image, 0, 0);
                 return canvas;
             });
 
         function newCanvas(domNode) {
             var canvas = document.createElement('canvas');
-            canvas.width = options.width || util.width(domNode);
-            canvas.height = options.height || util.height(domNode);
+            canvas.width = options.width || util.width(domNode) * 2;
+            canvas.height = options.height || util.height(domNode) * 2;
+            canvas.style.width = util.width(domNode);
+            canvas.style.height = util.height(domNode);
 
             if (options.bgcolor) {
                 var ctx = canvas.getContext('2d');
